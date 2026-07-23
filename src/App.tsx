1e6d7b0153a5a64eebb2c196dbe686abf8da1cbd
Navnath-Msquare <navnath.b@msquaresoftware.com>
2026-07-23 22:59:39 +0530

    all pages footer add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import FindJobs from "./pages/FindJobs";
 import ForEmployers from "./pages/ForEmployers";
@@ -10,25 +11,23 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/find-jobs" element={<FindJobs />} />
-
-        <Route path="/for-employers" element={<ForEmployers />} />
-
-        <Route path="/pricing" element={<Pricing />} />
-
-        <Route path="/about-us" element={<AboutUs />} />
-
-        <Route path="/contact-us" element={<ContactUs />} />
-
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/find-jobs" element={<FindJobs />} />
+          <Route path="/for-employers" element={<ForEmployers />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,15 @@ import Statistics from "../components/home/Statistics";
 import WhyChooseUs from "../components/home/WhyChooseUs";
 import HowItWorks from "../components/home/HowItWorks";
 import CTASection from "../components/home/CTASection";
-import Footer from "../components/layout/Footer";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
-
       <Statistics />
-
       <WhyChooseUs />
-
       <HowItWorks />
-
       <CTASection />
-
-      <Footer />
     </>
   );
 };
